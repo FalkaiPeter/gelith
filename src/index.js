69a9ae76e18eabd438bbf0952d21002732bd5215
FalkaiPeter <formal.pfalkai@gmail.com>
2022-05-26 17:11:44 +0200

    libs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 const yargs = require('yargs');
 const app = require('./app');
 const init = require('./init');
+const lib = require('./lib');
 
 async function main() {
   if (yargs.argv._[0] === 'init') await init();
   if (yargs.argv._[0] === 'app') await app();
+  if (yargs.argv._[0] === 'lib') await lib();
 }
 
 main();
